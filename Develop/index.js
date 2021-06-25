66d8f7fe4eeefdfdc317d7ee6e5def57782950c9
Danny Roubin <dannyroubin1@gmail.com>
2021-06-24 18:25:51 -0700

    built up almost the whole app, made it so you get a pretty good readme so far but I want to change the licence so you can write in which one you have and also allow the user to add in a video or image to their readme

--- Develop/index.js
+++ Develop/index.js
@@ -23,6 +23,11 @@ inquirer
     },
     {
       type: "input",
+      name: "usage",
+      message: "How do you use your app?",
+    },
+    {
+      type: "input",
       name: "license",
       message: "What is the licensing for the project?",
     },
@@ -57,35 +62,45 @@ inquirer
   });
 
 const generateMarkdown = function (answers) {
-  const md = `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      />
-      <title>Document</title>
-    </head>
-    <body>
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-          <p class="lead">I am from ${answers.location}.</p>
-          <h3>
-            Example heading <span class="badge badge-secondary">Contact Me</span>
-          </h3>
-          <ul class="list-group">
-            <li class="list-group-item">
-              My GitHub username is ${answers.github}
-            </li>
-            <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-          </ul>
-        </div>
-      </div>
-    </body>
-  </html>
+  const md = `
+  # ${answers.title}
+
+
+  ## table of contents
+  [App Description](#app-description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Licence](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+## App Description 
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This ReadMe is covered under the following license: ${answers.license}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.test}
+
+## Questions
+For questions, you can contact me either at my github ${answers.github} or you can reach me by email at ${answers.email} 
+
+
+![A screenshot of the app.](./assets/images/AppScreenshot.jpg)
+
+## Link to the App
+https://dannyroubin.github.io/Homework5_WorkDayScheduler/
   `;
 
   return md;
---
+++ Develop/readmegenerator.md
@@ -0,0 +1,40 @@
+
+  # readme generator
+
+
+  ## table of contents
+  [App Description](#app-description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Licence](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+## App Description 
+This project allows you to create readmes easily
+
+## Installation
+pull this file and do an npm , then run node index.js and you'll get prompts
+
+## Usage
+run node index.js to get prompts
+
+## License
+This ReadMe is covered under the following license: none
+
+## Contributing
+download the code and have fun
+
+## Tests
+I don't have any, just have fun with the code
+
+## Questions
+For questions, you can contact me either at my github [here] (https://github.com/DannyRoubin) or you can reach me by email at dannyroubin1@gmail.com 
+
+
+![A screenshot of the app.](./assets/images/AppScreenshot.jpg)
+
+## Link to the App
+https://dannyroubin.github.io/Homework5_WorkDayScheduler/
+  
